821c0311919cc3caf6ac66e3d12c6f28ba4afecc
Chous <kidminsk@yandex.ru>
2025-10-11 18:03:40 +0300

    Reapply "Отображение имени юзера в хедере"
    This reverts commit 0265cb33efc1456744dce6081b62d548a4b5906b.

--- src/main/java/org/chous/bets/config/WebSecurityConfig.java
+++ src/main/java/org/chous/bets/config/WebSecurityConfig.java
@@ -1,5 +1,7 @@
 package org.chous.bets.config;
 
+import lombok.AllArgsConstructor;
+import org.chous.bets.security.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -7,7 +9,6 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -15,22 +16,19 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 @EnableWebSecurity
 @EnableMethodSecurity
+@AllArgsConstructor
 public class WebSecurityConfig {
 
-    private final UserDetailsService userService;
-
-    public WebSecurityConfig(UserDetailsService userService) {
-        this.userService = userService;
-    }
+    private final CustomUserDetailsService userService;
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http    // todo под анонимным юзером не открывается /fixtures
                 .authorizeHttpRequests(authz -> authz
-                        .requestMatchers("/", "/registration", "/login",
-                                "/fixtures", "/tables/**", "/rules",
-                                "/activate/*", "/reset/*", "/reset-password", "/reset-form/*",
-                                "/archive/**")
+                        .requestMatchers("/", "/registration", "/login", "/activate/*",
+                                "/reset/*", "/reset-password", "/reset-form/*",
+                                "/fixtures", "/tables/**", "/rules", "/archive/**",
+                                "/css/*", "/img/**")
                         .permitAll()
                         .anyRequest().authenticated()
                 )
